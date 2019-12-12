7bb3fac2768b1a0a772d3b1876e11cbd1d712a2b
Taishawn King <taishawnking1@yahoo.com>
2019-12-12 22:55:21 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,11 +2,11 @@
  def speak_to_grandma(phrase)
 
  if phrase == "I LOVE YOU GRANDMA!"
-   puts 
+   puts "I LOVE YOU TOO PUMPKIN!"
    elsif phrase == phrase.upcase
    puts "NO, NOT SINCE 1938!"
  else 
-   "HUH?! SPEAK UP, SONNY!"
+   puts "HUH?! SPEAK UP, SONNY!"
  end
 
 # Whatever you say to grandma, she should respond with
