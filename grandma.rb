e7ba2042c7f1899fccb96fca1017e908618cfaab
Taishawn King <taishawnking1@yahoo.com>
2019-12-12 22:23:14 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,8 @@
 # Write a speak_to_grandma method.
  sayin = gets.chomp
  def speak_to_grandma(saying)
-   if 
+   if saying ==  "I LOVE YOU GRANDMA!"
+     puts 
  end
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
